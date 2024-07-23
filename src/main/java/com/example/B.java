51d683cc959a79b4d18e39af2c1a35624ac6bb84
rsgitdemo <rahulsharmabhardwaj2172@gmail.com>
2024-07-23 15:25:25 +0530

    Fixed the bud number-3569 null value not stored in the database

--- src/main/java/com/example/B.java
+++ src/main/java/com/example/B.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println("add new link in home page for payment gateway");
+        System.out.println("Fixed the bud number-3569 null value not stored in the database");
     }
 }
