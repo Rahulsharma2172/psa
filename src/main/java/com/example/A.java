2e27df45576d72dc1f3ab0bfe77cda42191d3fa5
rsgitdemo <rahulsharmabhardwaj2172@gmail.com>
2024-07-23 22:00:40 +0530

    Update code in all file.

--- src/main/java/com/example/A.java
+++ src/main/java/com/example/A.java
@@ -5,6 +5,6 @@ import org.w3c.dom.ls.LSOutput;
 public class A {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Master branch - Class A");
     }
 }
--- src/main/java/com/example/B.java
+++ src/main/java/com/example/B.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println("Fixed the bud number-3569 null value not stored in the database");
+        System.out.println("Master branch - class B");
     }
 }
