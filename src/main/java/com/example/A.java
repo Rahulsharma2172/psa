c767a204fda5f714a748709e4da6f3ee3618671a
rsgitdemo <rahulsharmabhardwaj2172@gmail.com>
2024-07-23 15:17:40 +0530

    defect fixed - 3569 - Fixed null values not to stored in database

--- src/main/java/com/example/A.java
+++ src/main/java/com/example/A.java
@@ -4,6 +4,7 @@ import org.w3c.dom.ls.LSOutput;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("Our first bug fix");
+
+        System.out.println("Hello world!");
     }
 }
