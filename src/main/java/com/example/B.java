42dcd07e14d35c102c000bd5bee5dabb8337e872
rsgitdemo <rahulsharmabhardwaj2172@gmail.com>
2024-07-23 22:17:27 +0530

    defect-fix in home page

--- src/main/java/com/example/B.java
+++ src/main/java/com/example/B.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class B {
     public static void main(String[] args) {
-        System.out.println("Master branch - class B");
+        System.out.println("defect-fix-branch - class B");
     }
 }
